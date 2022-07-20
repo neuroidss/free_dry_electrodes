950df2c17d3289c5125f62a7c3456e01ef7a0e74
dmitry-sukhoruchkin <dmitry.sukhoruchkin@gmail.com>
2022-07-20 18:54:20 +0300

    Add files via upload

---
+++ free_dry_electrodes_6i_6o/p8x8s1.0mm.sh
@@ -0,0 +1,2 @@
+PYTHONPATH=/usr/lib/kicad-nightly/lib/python3/dist-packages/ kikit panelize --layout 'rows: 8; cols: 8; space: 1.0mm' free_dry_electrodes_6i_6o.kicad_pcb free_dry_electrodes_6i_6o_p8x8s1.0mm.kicad_pcb
+
